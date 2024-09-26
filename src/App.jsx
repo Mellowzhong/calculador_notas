24f2c8fa248e2dc2054d5096a8ec9bc00edabb9e
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-09-26 11:04:14 -0300

    fix:fix the bug to calculate the grade

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,50 @@ import { useState } from 'react'
 import Nota from './Nota/views/Nota'
 
 function App() {
-  const [count, setCount] = useState(1);
+  const [grades, setGrades] = useState(1);
+  const [totalGrade, setTotalGrade] = useState(0);
+  const [simple, setSimple] = useState(0);
+
+  const add = () => setGrades(grades + 1)
+
+  const discount = () => {
+    if (grades > 1) {
+      setGrades(grades - 1)
+    }
+  }
+
+  const handleGradeChange = (grade) => {
+    setTotalGrade(prevTotalGrade => prevTotalGrade + parseFloat(grade));
+  }
+
+  const handleCalculate = () => {
+    setSimple((totalGrade / grades).toFixed(2));
+    setTotalGrade(0);
+  }
 
   return (
     <div className='grid'>
-      Cantidad de ramos
-      <div className='mb-5'>
-        {count}
-        <button className='ml-4' onClick={() => setCount(count + 1)}>Agregar</button>
-        <button className='ml-4' onClick={() => setCount(count - 1)}>Descontar</button>
+      <div className='grid mb-5 m-auto'>
+        <h1 className='m-auto'>Cantidad de ramos</h1>
+        <div className='m-auto'>
+          {grades}
+        </div>
+        <div className='flex'>
+          <button className='ml-4' onClick={() => discount()}>descontar</button>
+          <button className='ml-4' onClick={() => add()}>agregar</button>
+        </div>
       </div>
-      <label htmlFor="ramo">
-        Ramo
-        <input className="rounded-md border-2" type="text" name="ramo" id="ramo" />
-      </label>
-      {
-        Array.from({ length: count }).map(
-          (_, index) => (
-            <Nota key={index} />
+      <div className='flex'>
+        {
+          Array.from({ length: grades }).map(
+            (_, index) => (
+              <Nota key={index} onGradeChange={handleGradeChange} />
+            )
           )
-        )
-      }
+        }
+      </div>
+      <button onClick={handleCalculate}>Calcular</button>
+      suma : {totalGrade.toFixed(1)} - total : {simple}
     </div>
   )
 }
--- src/Nota/views/Nota.jsx
+++ src/Nota/views/Nota.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import PropTypes from 'prop-types';
 
-export default function Nota() {
+export default function Nota({ onGradeChange }) {
+    const [grade, setGrade] = useState(1);
+
+
+    const handleChange = (event) => {
+        const newValue = parseFloat(event.target.value);
+        setGrade(newValue);
+    }
+
+    const handeRelease = () => {
+
+        onGradeChange(grade);
+    }
+
     return (
-        <label htmlFor="nota">
-            Nota
-            <input className="rounded-md border-2" type="text" name="nota" id="nota" />
-        </label>
+        <div>
+            <form>
+                <label htmlFor="nota" className="grid">
+                    Nota
+                    <input
+                        className="rounded-md border-2"
+                        type="range"
+                        name="nota"
+                        id="nota"
+                        min={1}
+                        max={7}
+                        step={0.1}
+                        value={grade}
+                        onChange={handleChange}
+                        onMouseUp={handeRelease}
+                        onTouchEnd={handeRelease}
+                    />
+                    value : {grade.toFixed(1)}
+                    <button type="submit">Confirmar</button>
+                </label>
+            </form>
+        </div>
     )
+}
+
+Nota.propTypes = {
+    onGradeChange: PropTypes.func,
 }
